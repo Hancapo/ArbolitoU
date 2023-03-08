789738fa687875c8398a5a24a9169dcb82604993
Vicente <vicho0202@gmail.com>
2023-03-08 18:10:25 -0300

    Added UI files in a folder.

--- ArbolitoU/UI/MainWindow.xaml.cs
+++ ArbolitoU/UI/MainWindow.xaml.cs
@@ -12,17 +12,17 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Wpf.Ui.Controls.Window;
 
 namespace ArbolitoU
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : FluentWindow
     {
         public MainWindow()
         {
-            InitializeComponent();
         }
     }
 }
