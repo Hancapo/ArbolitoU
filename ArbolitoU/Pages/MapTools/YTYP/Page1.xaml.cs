0acdc68470384ff62032cd649f1a58b29fa7ff7a
Vicente <vicho0202@gmail.com>
2023-03-09 03:18:58 -0300

    Added more stuff #2

---
+++ ArbolitoU/Pages/MapTools/YTYP/Page1.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace ArbolitoU.Pages.Map_Tools.YTYP;
+
+public partial class Page1 : Page
+{
+    public Page1()
+    {
+        InitializeComponent();
+    }
+}
