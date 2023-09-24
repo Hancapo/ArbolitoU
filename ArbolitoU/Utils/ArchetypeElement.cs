1072befb6526ab364c85ce916033886f1bcb7b7e
Vicente <vicho0202@gmail.com>
2023-09-24 20:57:39 -0300

    Deleted old version

--- ArbolitoU/Utils/ArchetypeElement.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-using CodeWalker.GameFiles;
-namespace ArbolitoU.Utils;
-public class ArchetypeElement
-{
-    public List<MetaHash> archetypeNames { get; set; }
-    public string YtypName { get; set; }
-
-}
