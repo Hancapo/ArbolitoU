72cc8e45d2fbf7914a819fc4c7fe5f07fafb8fad
Vicente <vicho0202@gmail.com>
2023-09-24 21:06:03 -0300

    New Avalonia version

---
+++ ArbolitoU/MainWindow.axaml.cs
@@ -0,0 +1,15 @@
+using Avalonia.Controls;
+using FluentAvalonia.UI.Windowing;
+
+namespace ArbolitoU;
+
+public partial class MainWindow : AppWindow
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        
+    }
+    
+    
+}
