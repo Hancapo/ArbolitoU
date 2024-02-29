72fc97c8cf43f43c26c4a217037d6f8073b449f6
Vicente <vicho0202@gmail.com>
2024-02-29 03:53:25 -0300

    Minor changes

--- ArbolitoU/Windows/FileSelection.axaml.cs
+++ ArbolitoU/Windows/FileSelection.axaml.cs
@@ -1,10 +1,11 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using FluentAvalonia.UI.Windowing;
 
 namespace ArbolitoU.Windows;
 
-public partial class FileSelection : Window
+public partial class FileSelection : AppWindow
 {
     public FileSelection()
     {
